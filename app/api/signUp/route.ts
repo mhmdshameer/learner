814306992dec4cdc1f5a9c190838c2b165dc2c79
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-07-12 20:06:25 +0530

    jwt

--- app/api/signUp/route.ts
+++ app/api/signUp/route.ts
@@ -4,26 +4,36 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export async function POST(req: Request){
-    try {
-        const {name, email, password} = await req.json();
-        await connectToMongoDB();
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+    await connectToMongoDB();
 
-        const userExists = await User.findOne({email});
-        if(userExists){
-            return NextResponse.json({message: "User already exists"}, {status: 400});
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const newUser = new User({name, email, password: hashedPassword});
-
-        await newUser.save();
-
-        const token = jwt.sign({id: newUser._id, email: newUser.email}, process.env.JWT_SECRET!, {expiresIn: "1d"} );
-
-        return NextResponse.json({message: "User created successfully", user: newUser, token}, {status: 201});
-    } catch (error) {
-        console.log("Signup error: ", error);
-        return NextResponse.json({message: "Signup failed"}, {status: 500});
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+
+    const token = jwt.sign(
+      { id: newUser._id, email: newUser.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1d" }
+    );
+
+    return NextResponse.json(
+      { message: "User created successfully", user: newUser, token },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Signup error: ", error);
+    return NextResponse.json({ message: "Signup failed" }, { status: 500 });
+  }
 }
